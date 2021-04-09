a41f9014b97b50c7e3d25c1e4665e8026298893c
phucb <phuctbui@gmail.com>
2021-04-09 10:06:58 +0700

    Uses laest shopify api version

--- src/main/java/com/storakle/shopify/ShopifyApiClient.java
+++ src/main/java/com/storakle/shopify/ShopifyApiClient.java
@@ -8,106 +8,105 @@ public interface ShopifyApiClient {
   int MAXIMUM_RETURNED_RESULTS = 250;
 
   @RequestLine(
-      "GET /admin/customers.json?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
+      "GET /admin/api/2021-04/customers.json?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
   CustomerList getCustomers(
       @Param("limit") Integer limit,
       @Param("since-id") String sinceId,
       @Param("page") Integer page,
       @Param("fields") String fields);
 
-  @RequestLine("GET /admin/customers/count.json")
+  @RequestLine("GET /admin/api/2021-04/customers/count.json")
   Count getCustomersCount();
 
   @RequestLine(
-      "GET /admin/custom_collections.json?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
+      "GET /admin/api/2021-04/custom_collections.json?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
   CustomCollectionList getCustomCollections(
       @Param("limit") Integer limit,
       @Param("since-id") String sinceId,
       @Param("page") Integer page,
       @Param("fields") String fields);
 
-  @RequestLine("GET /admin/custom_collections/count.json")
+  @RequestLine("GET /admin/api/2021-04/custom_collections/count.json")
   Count getCustomCollectionsCount();
 
   @RequestLine(
-      "GET /admin/smart_collections.json?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
+      "GET /admin/api/2021-04/smart_collections.json?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
   SmartCollectionList getSmartCollections(
       @Param("limit") Integer limit,
       @Param("since-id") String sinceId,
       @Param("page") Integer page,
       @Param("fields") String fields);
 
-  @RequestLine("GET /admin/smart_collections/count.json")
+  @RequestLine("GET /admin/api/2021-04/smart_collections/count.json")
   Count getSmartCollectionsCount();
 
   @RequestLine(
-      "GET /admin/products.json?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
+      "GET /admin/api/2021-04/products.json?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
   ProductList getProducts(
       @Param("limit") Integer limit,
       @Param("since-id") String sinceId,
       @Param("page") Integer page,
       @Param("fields") String fields);
 
-  @RequestLine("GET /admin/products/count.json")
+  @RequestLine("GET /admin/api/2021-04/products/count.json")
   Count getProductsCount();
 
-  @RequestLine("GET /admin/collects.json?limit={limit}&page={page}&fields={fields}")
+  @RequestLine("GET /admin/api/2021-04/collects.json?limit={limit}&page={page}&fields={fields}")
   CollectList getCollects(
       @Param("limit") Integer limit, @Param("page") Integer page, @Param("fields") String fields);
 
-  @RequestLine("GET /admin/collects/count.json")
+  @RequestLine("GET /admin/api/2021-04/collects/count.json")
   Count getCollectsCount();
 
   @RequestLine(
-      "GET /admin/orders.json?limit={limit}&since_id={since-id}&created_at_min={createdAtMin}&page={page}&fields={fields}&status={status}&financial_status={financialStatus}&fulfillment_status={fullfillmentStatus}")
+      "GET /admin/api/2021-04/orders.json?limit={limit}&since_id={since-id}&created_at_min={createdAtMin}&fields={fields}&status={status}&financial_status={financialStatus}&fulfillment_status={fullfillmentStatus}")
   OrderList getOrders(
       @Param("limit") Integer limit,
       @Param("since-id") String sinceId,
       @Param("createdAtMin") String createdAtMin,
-      @Param("page") Integer page,
       @Param("fields") String fields,
       @Param("status") OrderStatusFilter status,
       @Param("financialStatus") FinancialStatus financialStatus,
       @Param("fullfillmentStatus") FullfillmentStatusFilter fullfillmentStatus);
 
-  @RequestLine("GET /admin/orders/count.json?status=any")
+  @RequestLine("GET /admin/api/2021-04/orders/count.json?status=any")
   Count getOrdersCount();
 
   @RequestLine(
-      "GET /admin/webhooks.json?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
+      "GET /admin/api/2021-04/webhooks.json?limit={limit}&since_id={since-id}&page={page}&fields={fields}")
   WebhookList getWebhooks(
       @Param("limit") Integer limit,
       @Param("since-id") String sinceId,
       @Param("page") Integer page,
       @Param("fields") String fields);
 
-  @RequestLine("GET /admin/webhooks/count.json")
+  @RequestLine("GET /admin/api/2021-04/webhooks/count.json")
   Count getWebhooksCount();
 
-  @RequestLine("GET /admin/orders/{orderId}/transactions.json?limit={limit}&since_id={since-id}")
+  @RequestLine("GET /admin/api/2021-04/orders/{orderId}/transactions.json?limit={limit}&since_id={since-id}")
   TransactionList getTransactions(
       @Param("orderId") String orderId,
       @Param("limit") Integer limit,
       @Param("since-id") String sinceId);
 
-  @RequestLine("GET /admin/orders/{orderId}/transactions/count.json")
+  @RequestLine("GET /admin/api/2021-04/orders/{orderId}/transactions/count.json")
   Count getTransactionsCount(@Param("orderId") String orderId);
 
-  @RequestLine("POST /admin/webhooks.json")
+  @RequestLine("POST /admin/api/2021-04/webhooks.json")
   Webhook createWebhook(Webhook webhook);
 
-  @RequestLine("POST /admin/recurring_application_charges.json")
+  @RequestLine("POST /admin/api/2021-04/recurring_application_charges.json")
   RecurringApplicationChargeResponse createRecurringApplicationCharge(
       RecurringApplicationChargeRequest request);
 
-  @RequestLine("DELETE /admin/recurring_application_charges/{chargeId}.json")
+  @RequestLine("DELETE /admin/api/2021-04/recurring_application_charges/{chargeId}.json")
   void cancelRecurringApplicationCharge(@Param("chargeId") String chargeId);
 
-  @RequestLine("POST /admin/recurring_application_charges/{chargeId}/activate.json")
+  @RequestLine("POST /admin/api/2021-04/recurring_application_charges/{chargeId}/activate.json")
   RecurringApplicationChargeResponse activateRecurringApplicationCharge(
       @Param("chargeId") String chargeId);
 
-  @RequestLine("GET /admin/recurring_application_charges/{chargeId}.json")
+  @RequestLine("GET /admin/api/2021-04/recurring_application_charges/{chargeId}.json")
   RecurringApplicationChargeResponse getRecurringApplicationCharge(
       @Param("chargeId") String chargeId);
 }
